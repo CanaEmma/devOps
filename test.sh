005af6d75711ab4ca58f750935809fbfd3c3650e
CanaEmma <cana.emma@outlook.fr>
2024-12-18 14:32:23 +0000

    fin de journée

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Créer un fichier nommé "example.txt"
+echo "Création du fichier example.txt..."
+touch example.txt
+
+# Lister les fichiers et dossiers dans le répertoire actuel
+echo "Voici la liste des fichiers et dossiers dans le répertoire actuel :"
+ls -l
+
+# Afficher "Hello World"
+echo "Hello World"
